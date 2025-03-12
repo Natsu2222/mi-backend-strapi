a6ce6c66a3a3d76cd63de2c2b528094159f328d3
Sam Hatami <samhatami@MacBook-Pro-di-Sam.local>
2025-03-12 15:44:40 +0100

    cloudinary2

--- config/middlewares.ts
+++ config/middlewares.ts
@@ -8,7 +8,7 @@ export default [
         useDefaults: true,
         directives: {
           "connect-src": ["'self'", "https:"],
-          "img-src": ["'self'", "data:", "blob:", "https://res.cloudinary.com/dughfp61j"],
+          "img-src": ["'self'", "data:", "blob:", "https://res.cloudinary.com/"],
           "media-src": ["'self'", "data:", "blob:"],
           upgradeInsecureRequests: null,
         },
